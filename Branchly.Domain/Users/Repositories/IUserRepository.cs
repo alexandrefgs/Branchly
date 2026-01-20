376b2caf4be1a18d3b85d16dbe257ff5f481487c
Alexandre <alexandrefagundes2014@hotmail.com>
2026-01-20 14:21:49 -0300

    feat(domain): adiciona contrato de repositório de usuários

---
+++ Branchly.Domain/Users/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Branchly.Domain.Users.ValueObjects;
+
+namespace Branchly.Domain.Users.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(UserId id, CancellationToken cancellationToken = default);
+    Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken = default);
+    Task AddAsync(User user, CancellationToken cancellationToken = default);
+}
